1025c1a08d9c6d216f6d0f72b12cfa42ac3e92c5
zhan81776075 <416810161@qq.com>
2019-04-25 15:25:24 +0800

    update

---
+++ FindInPartiallySortedMatrix.cpp
@@ -0,0 +1,129 @@
+// 面试题4：二维数组中的查找
+// 题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按
+// 照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个
+// 整数，判断数组中是否含有该整数。
+
+#include <cstdio>
+#include <vector>
+
+bool Find(std::vector<std::vector<int>>& matrix, int rows, int columns, int number)
+{
+	bool found = false;
+	if (!matrix.empty() && rows > 0 && columns > 0)
+	{
+		int row = 0;
+		int column = columns - 1;
+		while (row < rows && column >= 0)
+		{
+			if (matrix[row][column] == number)
+			{
+				found = true;
+				break;
+			}
+			else if (matrix[row][column] > number)
+				--column;
+			else
+				++row;
+		}
+	}
+	return found;
+}
+
+// ====================测试代码====================
+void Test(char* testName, std::vector<std::vector<int>>& matrix, int rows, int columns, int number, bool expected)
+{
+	if (testName != nullptr)
+		printf("%s begins: ", testName);
+
+	bool result = Find(matrix, rows, columns, number);
+	if (result == expected)
+		printf("Passed.\n");
+	else
+		printf("Failed.\n");
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数在数组中
+void Test1()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test1", matrix, 4, 4, 7, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数不在数组中
+void Test2()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test2", matrix, 4, 4, 5, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最小的数字
+void Test3()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test3", matrix, 4, 4, 1, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数是数组中最大的数字
+void Test4()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test4", matrix, 4, 4, 15, true);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最小的数字还小
+void Test5()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test5", matrix, 4, 4, 0, false);
+}
+
+//  1   2   8   9
+//  2   4   9   12
+//  4   7   10  13
+//  6   8   11  15
+// 要查找的数比数组中最大的数字还大
+void Test6()
+{
+	std::vector<std::vector<int>> matrix = { { 1, 2, 8, 9 },{ 2, 4, 9, 12 },{ 4, 7, 10, 13 },{ 6, 8, 11, 15 } };
+	Test("Test6", matrix, 4, 4, 16, false);
+}
+
+// 鲁棒性测试，输入空指针
+void Test7()
+{
+	std::vector<std::vector<int>> matrix = {};
+	Test("Test7", matrix, 0, 0, 16, false);
+}
+
+int main(int argc, char* argv[])
+{
+	Test1();
+	Test2();
+	Test3();
+	Test4();
+	Test5();
+	Test6();
+	Test7();
+
+	return 0;
+}
