22a550de7e5263f08e137de102bd08e932aa0d40
zhan81776075 <416810161@qq.com>
2019-05-04 19:27:37 +0800

    update

---
+++ QueueWithTwoStacks.cpp
@@ -0,0 +1,43 @@
+// 面试题9：用两个栈实现队列
+// 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
+// 和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
+
+#include "Queue.h"
+
+// ====================测试代码====================
+void Test(char actual, char expected)
+{
+    if(actual == expected)
+        printf("Test passed.\n");
+    else
+        printf("Test failed.\n");
+}
+
+int main(int argc, char* argv[])
+{
+    CQueue<char> queue;
+
+    queue.appendTail('a');
+    queue.appendTail('b');
+    queue.appendTail('c');
+
+    char head = queue.deleteHead();
+    Test(head, 'a');
+
+    head = queue.deleteHead();
+    Test(head, 'b');
+
+    queue.appendTail('d');
+    head = queue.deleteHead();
+    Test(head, 'c');
+
+    queue.appendTail('e');
+    head = queue.deleteHead();
+    Test(head, 'd');
+
+    head = queue.deleteHead();
+    Test(head, 'e');
+
+    return 0;
+}
+
