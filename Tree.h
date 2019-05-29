415940b2afd59259246fdb22de8e818aab3707f3
zhan81776075 <416810161@qq.com>
2019-05-29 20:01:57 +0800

    5.29

---
+++ Tree.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+struct TreeNode 
+{
+    int                       m_nValue;    
+    std::vector<TreeNode*>    m_vChildren;    
+};
+
+__declspec( dllexport ) TreeNode* CreateTreeNode(int value);
+__declspec( dllexport ) void ConnectTreeNodes(TreeNode* pParent, TreeNode* pChild);
+__declspec( dllexport ) void PrintTreeNode(const TreeNode* pNode);
+__declspec( dllexport ) void PrintTree(const TreeNode* pRoot);
+__declspec( dllexport ) void DestroyTree(TreeNode* pRoot);
